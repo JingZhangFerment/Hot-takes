06c16738ad71d03cd8edfb79f69779cd52e916c8
Jing ZHANG <jing.zhang.ferment@gmail.com>
2022-02-28 15:46:43 +0100

    démarrer le serveur backend

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
-const express = require ("express");
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express ();
+const app = express();
 
-app.use((req, res)=> {
- res.json({ message: "votre requête a bien été enregistré!" });
+//Connecter l'API au cluster MongoDB
+mongoose.connect("mongodb+srv://zhang-ferment:HOTTAKES2022@cluster0.8muze.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPINIONS "
+  );
+  next();
+});
+
+app.post("/api/sauces", (req, res, next) => {});
+
+app.get((req, res, next) => {
+  res.json({ message: "votre requête a bien été enregistré!" });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,55 @@
-//importer le package http de node qui nous permettra de créer un serveur 
-const http = require ("http");
-const app = require ("./app");
+const http = require("http");
+const app = require("./app");
 
+//renvoyer un port valide, qu'il soit fourni sous la forme d'un numéro ou d'une chaîne
+const normalizePort = (val) => {
+  const port = parseInt(val, 10);
 
-app.set("port", 3000);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
+};
+
+const port = normalizePort(process.env.Port || "3000");
+
+//sur quel port le serveur tourne
+app.set("port", port);
+
+//rechercher les différentes erreurs et les gèrer
+const errorHandler = (error) => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe" + address : "port" + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges.");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use.");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+//créer un serveur avec Node
 const server = http.createServer(app);
 
-server.listen (3000);
+server.on("error", errorHandler);
+server.on("listening", () => {
+  const address = server.address();
+  const bind = typeof address === "string" ? "pipe" + address : "port" + port;
+  console.log("listening on" + bind);
+});
+
+server.listen(port);
